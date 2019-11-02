6242122e89d2ab60c84bd75a97ac9813f9debd7f
suzuka <suzuka@WIN10>
2019-11-02 17:53:16 +0900

    プロジェクト ファイルを追加します。

---
+++ WebSocketServer/frmMain.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace WebSocketServer
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "開始";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLog.Location = new System.Drawing.Point(12, 41);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(458, 256);
+            this.txtLog.TabIndex = 1;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(93, 12);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "停止";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExit.Location = new System.Drawing.Point(395, 12);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "終了";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 307);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmMain";
+            this.Text = "WebSocketクライアントアプリ";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ WebSocketServer/frmMain.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace WebSocketServer
+{
+    public partial class frmMain : Form
+    {
+        private WebSocketSharp.Server.WebSocketServer server;
+
+        public class Lancher : WebSocketBehavior
+        {
+            protected override void OnMessage(MessageEventArgs e)
+            {
+                Send($"{DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")} サーバ：{e.Data}を受信");
+                
+                string receiveData = e.Data.ToUpper();
+                if (receiveData == "SAKURA")
+                {
+                    Process.Start(@"C:\Program Files (x86)\sakura\sakura.exe");
+                    Send($"{DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")} サーバ：サクラエディタを起動");
+                }
+            }
+        }
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            server.Start();
+            OutLog("サーバ待ち受け開始");
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            server = new WebSocketSharp.Server.WebSocketServer(8080);
+            server.AddWebSocketService<Lancher>("/");
+
+            OutLog("サーバ初期化完了");
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            server.Stop();
+            OutLog("サーバ停止");
+        }
+
+        public void OutLog(string msg)
+        {
+            txtLog.Text += $"{DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")} {msg}{Environment.NewLine}";
+        }
+    }
+}
